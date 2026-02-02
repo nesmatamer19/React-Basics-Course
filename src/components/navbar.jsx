480859d5ca71e259d10eb01bf742526aa2fc49d4
Nesma Tamer <nesma1900464@miuegypt.edu.eg>
2026-02-02 14:24:34 +0200

    implementing create browser router

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = props => {
                 <div className="flex justify-between items-center">
                     <div className="text-2xl font-bold">Navbar</div>
                     <ul className="flex space-x-6">
-                        <li><Link to="/home" className="hover:text-gray-300">Home</Link></li>
+                        <li><Link to="/" className="hover:text-gray-300">Home</Link></li>
                         <li><Link to="/about" className="hover:text-gray-300">About</Link></li>
                     </ul>
                 </div>
--- src/routing/Main.jsx
+++ src/routing/Main.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
-import { BrowserRouter , Route, Routes} from 'react-router-dom'
+import { BrowserRouter , createBrowserRouter, createRoutesFromElements, HashRouter, Route, RouterProvider, Routes} from 'react-router-dom'
 import Home from '../pages/home'
 import About from '../pages/about'  
 import Navbar from '../components/navbar'
+import { Outlet } from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    //takes an array of route elements
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+)
+function RootLayout() {
+  // layout can be sidebars, navbars, footers, etc
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  )
+}
 
 const Main = () => {
   return (
-    <div>
-      <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/home' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-      </Routes>
-      </BrowserRouter>   
-    </div>
+    <RouterProvider router ={router} />
   )
 }
 
