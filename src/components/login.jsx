c7da4b121c57a3222b9dd98da21c326e1280de76
nesmatamer19 <nesma1900464@miuegypt.edu.eg>
2026-02-17 11:11:00 +0200

    Access form value using generic function

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, use } from 'react';
 
 class Login extends Component {
-    state = {  } 
+    state = { 
+        username : "",
+        password : ""
+     } 
 
-    username = React.createRef();
 
     handleSubmit = e => {
-        const username = this.username.current.value
         e.preventDefault();
+    }
+
+    handleChange = e => {
+       let state = {...this.state}
+       state[e.currentTarget.name] = e.currentTarget.value;
+       this.setState( state);
     }
 
     render() { 
@@ -17,11 +24,11 @@ class Login extends Component {
             <form className='p-4' onSubmit={this.handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input ref={this.username} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <input name ="username" value ={this.state.username} onChange={this.handleChange} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
             </div>
             <div className="mb-3">
                 <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"/>
+                <input name ="password" value ={this.state.password} onChange={this.handleChange} type="password" className="form-control" id="exampleInputPassword1"/>
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
             </form>
